71e12b4a98b97bbe8dce9dd693ce86fe3dc9ea4c
heqingbao <qingbao.ho@gmail.com>
2019-02-10 00:54:22 +0800

    [天气预报微服务] init

--- weather-report-service/src/main/java/com/heqingbao/microweather/WeatherReportServiceApplication.java
+++ weather-report-service/src/main/java/com/heqingbao/microweather/WeatherReportServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicApplication {
+public class WeatherReportServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasicApplication.class, args);
+        SpringApplication.run(WeatherReportServiceApplication.class, args);
     }
 
 }
--- weather-report-service/src/main/java/com/heqingbao/microweather/controller/WeatherReportController.java
+++ weather-report-service/src/main/java/com/heqingbao/microweather/controller/WeatherReportController.java
@@ -1,7 +1,7 @@
 package com.heqingbao.microweather.controller;
 
-import com.heqingbao.microweather.service.CityDataService;
 import com.heqingbao.microweather.service.WeatherReportService;
+import com.heqingbao.microweather.vo.City;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/report")
 public class WeatherReportController {
-
-    @Autowired
-    private CityDataService cityDataService;
 
     @Autowired
     private WeatherReportService weatherReportService;
@@ -23,7 +23,16 @@ public class WeatherReportController {
     public ModelAndView getWeatherByCityId(@PathVariable("cityId") String cityId, Model model) {
         model.addAttribute("title", "天气预报");
         model.addAttribute("cityId", cityId);
-        model.addAttribute("cityList", cityDataService.listCity());
+        // TODO: 2019-02-10 改为由城市数据API微服务提供数据
+        // model.addAttribute("cityList", cityDataService.listCity());
+
+        List<City> cityList = new ArrayList<>();
+        City c = new City();
+        c.setId("101280601");
+        c.setName("深圳");
+        cityList.add(c);
+        model.addAttribute("cityList", cityList);
+
         model.addAttribute("report", weatherReportService.getDataByCityId(cityId));
         return new ModelAndView("weather/report", "reportModel", model);
     }
