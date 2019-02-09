78e07cce5389fda2329ea067e623c29f5eee3058
heqingbao <qingbao.ho@gmail.com>
2019-02-10 02:32:13 +0800

    集成 Eureka Client 实现服务的注册与发现

--- weather-city-service/src/main/java/com/heqingbao/microweather/WeatherCityServiceApplication.java
+++ weather-city-service/src/main/java/com/heqingbao/microweather/WeatherCityServiceApplication.java
@@ -2,8 +2,10 @@ package com.heqingbao.microweather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WeatherCityServiceApplication {
 
     public static void main(String[] args) {
